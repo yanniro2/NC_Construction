5117dbf7465b0e5dad39b9ac04ed3f1ce34d37b5
Niroyan Kugaruban <48611787+yanniro2@users.noreply.github.com>
2023-06-09 05:01:05 +0530

    payment is done

--- src/Pages/Payment.js
+++ src/Pages/Payment.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import { useLocation } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import { auth } from '../firebase';
 function Payment({ stocksPayment, logisticPayment })
 {
     const total = stocksPayment + logisticPayment;
@@ -7,52 +9,71 @@ function Payment({ stocksPayment, logisticPayment })
     const details = location.state?.details;
     // const { price } = props.location.state;
 
+    const [user, setUser] = useState(null);
+
+    useEffect(() =>
+    {
+        const unsubscribe = auth.onAuthStateChanged((user) =>
+        {
+            setUser(user);
+        });
+
+        return () =>
+        {
+            unsubscribe();
+        };
+    }, []);
+
     return (
-        <div className='w-full h-full py-[6rem] px-5 flex justify-center flex-col'>
-            <div className='text-center text-[1.5rem] font-xl font-open uppercase p-5 text-dark-blue flex justify-between w-full'>
-                <header className='text-[2rem] font-xl'>Payment</header>
-                <div className='btn bg-blue text-white capitalize'
-                >
-                    {/* total {price}$ */} total  {total}$
+        <div className='w-full h-full py-[6rem] px-5 flex justify-center flex-col relative'>
+            {user ? (<>
+                <div className='text-center text-[1.5rem] font-xl font-open uppercase p-5 text-dark-blue flex justify-between w-full'>
+                    <header className='text-[2rem] font-xl'>Payment</header>
+                    <div className='btn bg-blue text-white capitalize'
+                    >
+                        {/* total {price}$ */} total  {total}$
+                    </div>
                 </div>
-            </div>
-            <div className='w-full flex justify-center'>
-                <form className='w-1/2 h-full p-5'>
-                    <div class="mb-4">
-                        <label class="label" for="name">
-                            Name on Card
-                        </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="John Doe" />
-                    </div>
-                    <div class="mb-4">
-                        <label class="label" for="email">
-                            Email
-                        </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="john@example.com" />
-                    </div>
-                    <div class="mb-4">
-                        <label class="label" for="card">
-                            Card Number
-                        </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="card" type="text" placeholder="4242 4242 4242 4242" />
-                    </div>
-                    <div class="mb-4">
-                        <label class="label" for="expiry">
-                            Expiry Date
-                        </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="expiry" type="text" placeholder="MM/YY" />
-                    </div>
-                    <div class="mb-4">
-                        <label class="label" for="cvv">
-                            CVV
-                        </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="cvv" type="text" placeholder="123" />
-                    </div>
-                    <button class="bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
-                        Pay Now
-                    </button>
-                </form>
-            </div>
+                <div className='w-full flex justify-center'>
+                    <form className='w-1/2 h-full p-5'>
+                        <div class="mb-4">
+                            <label class="label" for="name">
+                                Name on Card
+                            </label>
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="name" type="text" placeholder="John Doe" />
+                        </div>
+                        <div class="mb-4">
+                            <label class="label" for="email">
+                                Email
+                            </label>
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="john@example.com" />
+                        </div>
+                        <div class="mb-4">
+                            <label class="label" for="card">
+                                Card Number
+                            </label>
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="card" type="text" placeholder="4242 4242 4242 4242" />
+                        </div>
+                        <div class="mb-4">
+                            <label class="label" for="expiry">
+                                Expiry Date
+                            </label>
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="expiry" type="text" placeholder="MM/YY" />
+                        </div>
+                        <div class="mb-4">
+                            <label class="label" for="cvv">
+                                CVV
+                            </label>
+                            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="cvv" type="text" placeholder="123" />
+                        </div>
+                        <button class="bg-blue hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                            Pay Now
+                        </button>
+                    </form>
+                </div></>) : (<div className='w-screen h-screen absolute flex items-center justify-center top-0 left-0 right-0 bottom-0'>
+
+                    <h1 className='h2'>You must  <Link to="/login" className='text-[red] underline'>log in</Link>  to access this content.</h1></div>)}
+
 
 
         </div>
--- src/Pages/Reciept.js
+++ src/Pages/Reciept.js
@@ -1,19 +1,24 @@
-// import React from 'react'
-
-// function Reciept()
-// {
-//     return (
-//         <div className='w-full h-full py-[6rem] px-5'>Reciept</div>
-//     )
-// }
-
-// export default Reciept
-
-
-import React from 'react';
+import React from 'react'
+import { useEffect, useState } from 'react';
+import { useLocation, Link } from 'react-router-dom';
+import { auth } from '../firebase';
 
 function Reciept()
 {
+    const [user, setUser] = useState(null);
+
+    useEffect(() =>
+    {
+        const unsubscribe = auth.onAuthStateChanged((user) =>
+        {
+            setUser(user);
+        });
+
+        return () =>
+        {
+            unsubscribe();
+        };
+    }, []);
     const data = {
         project: 'NC New Scall Project',
         manager: 'John Doe',
@@ -27,46 +32,49 @@ function Reciept()
 
     return (
         <div className='w-full h-full py-[6rem] px-5 font-open'>
-            <div className='text-[1.5rem] font-xl font-open uppercase p-5 text-dark-blue flex justify-between w-full'>
-                <header className='text-[2rem] font-xl'>
-                    {data.project} Report
-                </header>
-                <button className='btn '>Print</button>
-            </div>
-            <div className='p-5'>
-                <p className="font-bold">Project Manager:</p>
-                <p>{data.manager}</p>
-                <h1 className="text-2xl font-bold mb-4"></h1>
-                <div className="grid grid-cols-2 gap-4 mb-4">
-                    <div>
-                        <p className="font-bold">Start Date:</p>
-                        <p>{data.startDate}</p>
-                    </div>
-                    <div>
-                        <p className="font-bold">End Date:</p>
-                        <p>{data.endDate}</p>
-                    </div>
-                    <div>
-                        <p className="font-bold">Progress:</p>
-                        <p>{data.progress}%</p>
-                    </div>
-                    <div>
-                        <p className="font-bold">Budget:</p>
-                        <p>${data.budget}</p>
-                    </div>
-                    <div>
-                        <p className="font-bold">Expenses:</p>
-                        <p>${data.expenses}</p>
-                    </div>
-                    <div>
-                        <p className="font-bold">Revenue:</p>
-                        <p>${data.revenue}</p>
-                    </div>
+            {user ? (<>
+                <div className='text-[1.5rem] font-xl font-open uppercase p-5 text-dark-blue flex justify-between w-full'>
+                    <header className='text-[2rem] font-xl'>
+                        {data.project} Report
+                    </header>
+                    <button className='btn '>Print</button>
                 </div>
-                <div className="border-t border-gray-300 pt-4">
+                <div className='p-5'>
+                    <p className="font-bold">Project Manager:</p>
+                    <p>{data.manager}</p>
+                    <h1 className="text-2xl font-bold mb-4"></h1>
+                    <div className="grid grid-cols-2 gap-4 mb-4">
+                        <div>
+                            <p className="font-bold">Start Date:</p>
+                            <p>{data.startDate}</p>
+                        </div>
+                        <div>
+                            <p className="font-bold">End Date:</p>
+                            <p>{data.endDate}</p>
+                        </div>
+                        <div>
+                            <p className="font-bold">Progress:</p>
+                            <p>{data.progress}%</p>
+                        </div>
+                        <div>
+                            <p className="font-bold">Budget:</p>
+                            <p>${data.budget}</p>
+                        </div>
+                        <div>
+                            <p className="font-bold">Expenses:</p>
+                            <p>${data.expenses}</p>
+                        </div>
+                        <div>
+                            <p className="font-bold">Revenue:</p>
+                            <p>${data.revenue}</p>
+                        </div>
+                    </div>
+                    <div className="border-t border-gray-300 pt-4">
 
-                </div>
-            </div>
+                    </div>
+                </div></>) : (<div className='w-screen h-screen absolute flex items-center justify-center top-0 left-0 right-0 bottom-0'>
+                    <h1 className='h2'>You must  <Link to="/login" className='text-[red] underline'>log in</Link>  to access this content.</h1></div>)}
+
         </div>
     );
 }
